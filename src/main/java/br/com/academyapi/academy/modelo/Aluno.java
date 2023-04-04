239e587d07759690d3bf1e4b007970b15e8dd703
Sergio j Silva <juniosergio034@gmail.com>
2023-04-04 08:27:31 -0300

    implementando o Banco de dados

---
+++ src/main/java/br/com/academyapi/academy/modelo/Aluno.java
@@ -0,0 +1,62 @@
+package br.com.academyapi.academy.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Alunos")
+public class Aluno{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+    private String nome;
+    private int idade;
+
+
+    /**
+     * @return int return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return int return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
---
+++ src/test/java/br/com/academyapi/academy/AcademyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academyapi.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
